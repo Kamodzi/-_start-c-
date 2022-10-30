df2a80a89007b348a11c406d3cfe78f1917bb5c6
Kamodzi <113838638+Kamodzi@users.noreply.github.com>
2022-10-30 13:25:07 +0300

    Add files via upload

---
+++ Перевод в верхний регистр.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char  chLetter('a');
+
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите букыу нижнего регистра" << endl;
+
+	cin >> chLetter;
+
+	chLetter = chLetter - 32;
+
+	cout << "Эта же буква в новом регистре\n" << chLetter << endl;
+	
+	return 0;
+
+
+
+
+
+}
---
+++ деления числа на разряды путем строк.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+using namespace std;
+
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+
+	vector < string > names{{"Первая","Вторая","Третья","Четвертая","Пятая"}};
+
+	int number;
+
+	cout << "Введите число " << endl;
+
+	cin >> number;
+
+	string  s_number = to_string(number);
+
+	if (s_number.size() > names.size()) {
+
+		cout << "Максимальное количество чисел: " << names.size() << '\n';
+
+		return -1;
+
+	}
+	
+	for (size_t i = 0; i < s_number.size(); i++) {
+
+		cout << names[i] << " цифра числа " << s_number[i] << "\n";
+	}
+	return 0;
+	
+
+}
+
